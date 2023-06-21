82a902fcc14a489b84dd444fab1ae44098629fab
Julia <Julia_rum@mail.ru>
2023-06-21 17:08:06 +0300

    Вывод четных чисел

---
+++ Example4/Program.cs
@@ -0,0 +1,14 @@
+﻿// Задача 8: Напишите программу, которая на вход принимает число (N), а на выходе показывает все чётные числа от 1 до N.
+// 5 -> 2, 4
+// 8 -> 2, 4, 6, 8
+
+Console.WriteLine("Введите целое число N: ");
+int num = int.Parse(Console.ReadLine());
+Console.Write($"{num} -> ");
+for (int i = 1; i <= num; i++)
+{
+    if (i%2 == 0 && (i+1) < num )
+        Console.Write($"{i},");
+    if (i%2 == 0 && (i+1) >= num )
+        Console.Write($"{i}");
+}
