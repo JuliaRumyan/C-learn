381d838864b83a74a5506831492302e3c90e4a2c
Julia <Julia_rum@mail.ru>
2023-06-21 16:01:35 +0300

    Max from 3 numbers

--- Example2/Program.cs
+++ Example2/Program.cs
@@ -1,2 +1,22 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿//Задача 4: Напишите программу, которая принимает на вход три числа и выдаёт максимальное из этих чисел.
+// 2, 3, 7 -> 7
+// 44 5 78 -> 78
+// 22 3 9 -> 22
+
+Console.WriteLine("Введите первое целое число: ");
+int num1 = int.Parse(Console.ReadLine());
+Console.WriteLine("Введите второе целое число: ");
+int num2 = int.Parse(Console.ReadLine());
+Console.WriteLine("Введите третье целое число: ");
+int num3 = int.Parse(Console.ReadLine());
+int max=num1;
+if (num1>max)
+    max=num1;
+if (num2>max)   
+    max=num2;
+else
+    max=num3;
+Console.Write ($"Больше число: {max}");
+
+
+
