35372332f7d5cb671a32934864f6e46a0bdd2e0f
Julia <Julia_rum@mail.ru>
2023-06-26 22:39:18 +0300

    new solution of example2

--- Example2/Program.cs
+++ Example2/Program.cs
@@ -1,22 +1,23 @@
 ﻿//Задача 4: Напишите программу, которая принимает на вход три числа и выдаёт максимальное из этих чисел.
 // 2, 3, 7 -> 7
 // 44 5 78 -> 78
-// 22 3 9 -> 22
+// 22 3 9 -> 22 Разделяйте пробелами имена переменных и действия. Вы присвоили переменной max значение num1. Очевидно, что их значения будут равны. Тогда зачем их сравнивать?)
+// Достаточно просто организовать неполное ветвление из одного блока if, где Вы сравниваете max и num2
+
+
 
 Console.WriteLine("Введите первое целое число: ");
-int num1 = int.Parse(Console.ReadLine());
+int num1 = int.Parse(Console.ReadLine()!);
 Console.WriteLine("Введите второе целое число: ");
-int num2 = int.Parse(Console.ReadLine());
+int num2 = int.Parse(Console.ReadLine()!);
 Console.WriteLine("Введите третье целое число: ");
-int num3 = int.Parse(Console.ReadLine());
-int max=num1;
-if (num1>max)
-    max=num1;
-if (num2>max)   
-    max=num2;
-else
-    max=num3;
-Console.Write ($"Больше число: {max}");
+int num3 = int.Parse(Console.ReadLine()!);
+int max = num1;
+if (num2 > max)
+    max = num2;
+if (num3 > max)
+    max = num3;
+Console.Write($"Больше число: {max}");
 
 
 
