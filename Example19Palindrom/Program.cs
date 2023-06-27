451ff2dec35c8aa9cc8a3e7e0f90e0bf95bd2a4d
Julia <Julia_rum@mail.ru>
2023-06-27 17:32:35 +0300

    change folder name

---
+++ Example19Palindrom/Program.cs
@@ -0,0 +1,29 @@
+﻿// Задача 19
+// Напишите программу, которая принимает на вход пятизначное число и проверяет, является ли оно палиндромом.
+// 14212 -> нет
+// 12821 -> да
+// 23432 -> да
+
+int InputNum(string message)
+{
+    Console.Write(message);
+    return int.Parse(Console.ReadLine()!);
+}
+int ReverseNum(int number)
+{
+    int reversedNum = 0;
+    while (number !=0)
+    {
+        reversedNum = reversedNum * 10 +number % 10;
+        number /=10;
+    }
+    return reversedNum;
+}
+bool Palindrom(int num,int rev)
+{
+    return num == rev;
+}
+int num = InputNum("Введите пятизначное число: ");
+int rev = ReverseNum(num);
+bool res = Palindrom(num,rev);
+Console.WriteLine(res);
