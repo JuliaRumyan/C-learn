66273ade41c1b79bda64f850b4f89968acde390b
Julia <Julia_rum@mail.ru>
2023-06-28 19:27:41 +0300

    Array of 8 elements

---
+++ Example29Array/Program.cs
@@ -0,0 +1,23 @@
+﻿// Задача 29: Напишите программу, которая задаёт массив из 8 элементов и выводит их на экран.
+// 1, 2, 5, 7, 19 -> [1, 2, 5, 7, 19]
+// 6, 1, 33 -> [6, 1, 33]
+
+
+void FillArray(int[] array)
+{
+    for (int i = 0; i <= 7; i++)
+        array[i] = new Random().Next(-10, 10);
+}
+
+void PrintArray(int[] array)
+{
+    for (int i = 0; i <= 7; i++)
+        Console.Write($"{array[i]} ");
+}
+
+int[] array = new int[8];
+
+FillArray(array);
+PrintArray(array);
+
+
