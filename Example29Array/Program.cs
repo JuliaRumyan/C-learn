52e97cf7c083b4748e02960e9decb4d1cf624b38
Julia <Julia_rum@mail.ru>
2023-06-28 22:21:41 +0300

    Added [] and  comments to lines

--- Example29Array/Program.cs
+++ Example29Array/Program.cs
@@ -3,7 +3,7 @@
 // 6, 1, 33 -> [6, 1, 33]
 
 
-void FillArray(int[] array)
+void FillArray(int[] array) // метод для получения случайных значений массива
 {
     for (int i = 0; i <= 7; i++)
         array[i] = new Random().Next(-10, 10);
@@ -11,13 +11,18 @@ void FillArray(int[] array)
 
 void PrintArray(int[] array)
 {
+    Console.Write("["); // вывод скобки массива
     for (int i = 0; i <= 7; i++)
-        Console.Write($"{array[i]} ");
+    {
+        Console.Write($"{array[i]}, "); // вывод значения массива
+    }
+    Console.Write($"{array[7]}"); // вывод значения массива без запятой
+    Console.Write("]"); // вывод скобки массива
 }
-
 int[] array = new int[8];
+FillArray(array);// заполнение массива случайными числами
+PrintArray(array); // вывод массива
 
-FillArray(array);
-PrintArray(array);
-
-
+// можно сократить так:
+// int[] array = {1,2,3,4,5};
+// Console.WriteLine($"[{string.Join(", ",array)}]");
