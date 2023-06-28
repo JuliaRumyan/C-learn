2675a112c75fc551ee720ab6bfc07b1cba243410
Julia <Julia_rum@mail.ru>
2023-06-28 17:35:38 +0300

    exponentiation A,B

---
+++ Example25Exponentiation/Program.cs
@@ -0,0 +1,17 @@
+﻿// Задача 25: Напишите цикл, который принимает на вход два числа (A и B) и возводит число A в натуральную степень B.
+// 3, 5 -> 243 (3⁵)
+// 2, 4 -> 16
+
+int InputNum(string message)
+{
+    Console.WriteLine(message);
+    int num = int.Parse(Console.ReadLine()!);
+    return num;
+}
+
+int num1 = InputNum("Введите целое число A");
+int num2 = InputNum("Введите целое число B");
+
+Console.Write($"Число А в степени В: {Math.Pow(num1, num2)}");
+
+    
