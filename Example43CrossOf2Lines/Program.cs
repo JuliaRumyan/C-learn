96c7ac530ddb5d3c1a3017c8f5717793ef3cd9b1
Julia <Julia_rum@mail.ru>
2023-07-03 20:38:07 +0300

    added function to example 43 cross of 2 lines coordinates

--- Example43CrossOf2Lines/Program.cs
+++ Example43CrossOf2Lines/Program.cs
@@ -3,16 +3,16 @@
 
 // b1 = 2, k1 = 5, b2 = 4, k2 = 9 -> (-0,5; -0,5)
 
-Console.Write("Введите  b1: ");
-var b1 = Convert.ToDouble(Console.ReadLine()!);
-Console.Write("Введите b2: ");
-var b2 = Convert.ToDouble(Console.ReadLine()!);
-Console.Write("Введите k1: ");
-var k1 = Convert.ToDouble(Console.ReadLine()!);
-Console.Write("Введите k2: ");
-var k2 = Convert.ToDouble(Console.ReadLine()!);
- 
+double InputNum(string message)
+{
+    Console.Write(message); //выводим приглашение ко вводу
+    return double.Parse(Console.ReadLine()!); // приводим к числу и возвращаем результат
+}
+
+var b1 = InputNum ("Введите число b1: ");
+var b2 = InputNum ("Введите число b2: ");
+var k1 = InputNum ("Введите число k1: ");
+var k2 = InputNum ("Введите число k2: ");
 var y = (b2*k1 - b1* k2) / (k1 - k2);
 var x = (y-b1) / k1;
- 
 Console.WriteLine($"Пересечение  рямых в точке с координатами: ({x};{y})");
