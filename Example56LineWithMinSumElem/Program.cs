04837ffdcfaa2d2d0a15836e8dedbde0bb51e4ea
Julia <Julia_rum@mail.ru>
2023-07-15 00:50:50 +0300

    Sum of each line in Array

---
+++ Example56LineWithMinSumElem/Program.cs
@@ -0,0 +1,42 @@
+﻿// Задача 56: Задайте прямоугольный двумерный массив. Напишите программу, которая будет находить строку с наименьшей суммой элементов.
+// Например, задан массив:
+// 1 4 7 2
+// 5 9 2 3
+// 8 4 2 4
+// 5 2 6 7
+// Программа считает сумму элементов в каждой строке и выдаёт номер строки с наименьшей суммой элементов: 1 строка
+
+void Fill2DArray(int[,] array, int min, int max)
+{
+    Random rnd = new Random();
+    for (int i = 0; i < array.GetLength(0); i++)
+        for (int j = 0; j < array.GetLength(1); j++)
+            array[i, j] = rnd.Next(min, max + 1);
+}
+
+void Print2DArray(int[,] array)
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+            Console.Write($"{array[i, j]}\t");
+        Console.WriteLine();
+    }
+}
+
+void LineWithMinSumm(int[,] array)
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        int sumLine = 0;
+        for (int j = 0; j < array.GetLength(1); j++)
+            sumLine += array[i, j]; 
+        Console.Write($"Сумма по строке: {sumLine}\n");
+    }
+}
+
+int[,] myArray = new int[4, 5];
+Fill2DArray(myArray, 0, 10);
+Print2DArray(myArray);
+Console.WriteLine();
+LineWithMinSumm (myArray);
